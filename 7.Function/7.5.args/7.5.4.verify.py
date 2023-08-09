3f2732d7edc7bc3c5f0e269445aadee344eac262
gennady <genakoganovich@gmail.com>
2023-08-09 18:48:23 +0300

    0012: isolate done

--- 7.Function/7.5.args/7.5.4.verify.py
+++ 7.Function/7.5.args/7.5.4.verify.py
@@ -1,2 +1,21 @@
-def verify():
-    pass
+def is_isolate(*tple):
+    return tple.count(1) <= 1
+
+
+def verify(inpt):
+    for i in range(len(inpt) - 1):
+        for j in range(len(inpt[i]) - 1):
+            if not is_isolate(inpt[i][j], inpt[i][j+1], inpt[i+1][j], inpt[i+1][j+1]):
+                return False
+    return True
+
+
+# Пример ввода
+input_table = [
+    [1, 0, 1],
+    [0, 1, 1],
+    [1, 0, 1]
+]
+
+# Вызываем функцию verify и выводим результат
+print(verify(input_table))
